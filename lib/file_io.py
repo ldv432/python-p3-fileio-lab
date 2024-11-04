8eee19d1bc566ab244b8d34b67f47f84ec80f01e
ldv432 <ldv432@gmail.com>
2024-11-03 20:39:28 -0700

    file management simulator

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
+     # Add .txt extension to the file name
+    file_name = f"{file_name}.txt"
+    # Open file in write mode and write content
+    with open(file_name, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+     # Add .txt extension to the file name
+    file_name = f"{file_name}.txt"
+    # Open file in append mode and add content
+    with open(file_name, 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    # Add .txt extension to the file name
+    file_name = f"{file_name}.txt"
+    # Open file in read mode and return its content
+    with open(file_name, 'r') as file:
+        return file.read()
